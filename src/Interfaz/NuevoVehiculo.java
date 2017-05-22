a9a35e0243ff1540076a79b1377a8318bd71236b
Alfonso <Alfonso@192.168.1.196>
2017-05-22 13:05:40 +0200

    Creado Interfaces Login, Nuevo Vehiculo y Pantalla Principal

---
+++ src/Interfaz/NuevoVehiculo.java
@@ -0,0 +1,43 @@
+package Interfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class NuevoVehiculo {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					NuevoVehiculo window = new NuevoVehiculo();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public NuevoVehiculo() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
