ff1fa69e7d5ebce52fec8ded71d0f0783d6d502e
sergio <sergio_pla_11@hotmail.com>
2017-05-22 23:37:46 +0200

    Comentarios borrados

--- src/res/Constants.java
+++ src/res/Constants.java
@@ -4,6 +4,5 @@ import java.util.regex.Pattern;
 
 public class Constants {
 	public static String go = "Iniciar sesion";
-	// public static String CONSTRAINT_CONTRASENIA =
 	public static Pattern CONSTRAINT_CONTRASENIA = Pattern.compile("^[0-9]{1,4}$");
 }
