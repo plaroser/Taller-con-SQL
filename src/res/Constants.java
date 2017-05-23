90cf12edf5f8801ce0005da660a11ad45662ee40
sergio <sergio_pla_11@hotmail.com>
2017-05-23 18:41:59 +0200

    Regex matricula añadido

--- src/res/Constants.java
+++ src/res/Constants.java
@@ -5,4 +5,6 @@ import java.util.regex.Pattern;
 public class Constants {
 	public static String go = "Iniciar sesion";
 	public static Pattern CONSTRAINT_CONTRASENIA = Pattern.compile("^[0-9]{1,4}$");
+	public static Pattern REGEX_MATRICULA = Pattern
+			.compile("^([A-Z]{1,2})([0-9]{4})([A-Z]{1,2})$|^([0-9]{4})([A-Z]{3})$");
 }
