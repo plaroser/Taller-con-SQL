77660479c98e0d3ef884fe3ff79f8a4d2f69bf47
sergio <sergio_pla_11@hotmail.com>
2017-05-23 19:03:53 +0200

    Pantalla principal ordenada

--- src/UI/PantallaPrincipal.java
+++ src/UI/PantallaPrincipal.java
@@ -5,18 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import Models.Vehiculo;
 
 import javax.swing.JButton;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Collection;
 
 public class PantallaPrincipal {
 
 	private JFrame frame;
 	private JButton btnBV;
 	private JButton btnNV;
+	private Collection<Vehiculo> listaActual;
 
 	public JFrame getFrame() {
 		return frame;
@@ -25,7 +28,7 @@ public class PantallaPrincipal {
 	public void setFrame(JFrame frame) {
 		this.frame = frame;
 	}
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -46,53 +49,46 @@ public class PantallaPrincipal {
 	 * Create the application.
 	 */
 	public PantallaPrincipal() {
-		Componentes();
-		BV();
-		NV();
+		initialize();
+		setComponetProperties();
+		setComponentAdapters();
+	}
+
+	private void setComponentAdapters() {
+		btnNV.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				UI.Vehiculo Ventana = new UI.Vehiculo();
+				Ventana.getFrame().setVisible(true);
+				frame.dispose();
+
+			}
+		});
+	}
+
+	private void setComponetProperties() {
+		frame.setBounds(100, 100, 519, 232);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Pantalla Principal");
+		frame.getContentPane().setLayout(null);
+
+		btnBV.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		btnBV.setBounds(32, 59, 194, 76);
+		frame.getContentPane().add(btnBV);
+
+		btnNV.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		btnNV.setBounds(265, 59, 194, 76);
+		frame.getContentPane().add(btnNV);
 	}
 
 	/**
 	 * Componentes principales
 	 */
-	private void Componentes() {
+	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 519, 232);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Pantalla Principal");
-		frame.getContentPane().setLayout(null);
-		
-		
-	}
-	
-	/**
-	 * Boton Buscar Vehiculo
-	 */
-	
-	private void BV(){
 		btnBV = new JButton("Buscar Vehiculo");
-		btnBV.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
-		btnBV.setBounds(32, 59, 194, 76);
-		frame.getContentPane().add(btnBV);
-	}
-	
-	/**
-	 * Boton Nuevo Vehiculo
-	 */
-	
-	private void NV(){
 		btnNV = new JButton("Nuevo Vehiculo");
-		btnNV.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
-		btnNV.setBounds(265, 59, 194, 76);
-		frame.getContentPane().add(btnNV);
-		btnNV.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e){
-					Vehiculo Ventana = new Vehiculo();
-					Ventana.getFrame().setVisible(true);
-					frame.dispose();
-			
-				
-			}
-		});
+
 	}
+
 }
