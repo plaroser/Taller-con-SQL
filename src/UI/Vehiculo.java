1e230ae66cb0bb288ab7e5a6132c285cbf4de84a
sergio <sergio_pla_11@hotmail.com>
2017-05-24 12:56:34 +0200

    Codigo ordenado y metodo limpiar creado

--- src/UI/Vehiculo.java
+++ src/UI/Vehiculo.java
@@ -52,9 +52,6 @@ public class Vehiculo {
 	private JButton buttonLimpiar;
 	private JLabel lblCV;
 	private Models.Vehiculo vehiculoSeleccionado;
-	
-	
-	
 
 	public Collection<Models.Vehiculo> getListaVehiculo() {
 		return listaVehiculo;
@@ -92,174 +89,165 @@ public class Vehiculo {
 	 * Create the application.
 	 */
 	public Vehiculo(Collection<Models.Vehiculo> listaVehiculo, Models.Vehiculo v) {
-		this.listaVehiculo=listaVehiculo;
-		this.vehiculoSeleccionado=v;
-		Componentes();
-		Botones();
-		setComponentProperties();
+		this.listaVehiculo = listaVehiculo;
+		this.vehiculoSeleccionado = v;
+		initialize();
 	}
 
-	/**
-	 * Componentes del Frame
-	 */
-	private void Componentes() {
+	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 555, 493);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Vehiculo");
-		frame.getContentPane().setLayout(null);
-		
-		
-		
-	}
-	
-
-	/**
-	 */
-	private void Botones(){
 		btnCliente = new JButton("Cliente");
-		btnCliente.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
-		btnCliente.setBounds(398, 66, 135, 78);
-		frame.getContentPane().add(btnCliente);
+		btnReparacion = new JButton("Reparar");
+		btnGuardar = new JButton("Guardar");
+		textNombre = new JTextField();
+		buttonLimpiar = new JButton("Limpiar");
+		btnEditar = new JButton("Editar");
+		textColor = new JTextField();
+		lblMatricula_1 = new JLabel("Matricula:");
+		lblMarca = new JLabel("Marca:");
+		textMarca = new JTextField();
+		lblModelo = new JLabel("Modelo:");
+		textModelo = new JTextField();
+		lblCV = new JLabel("CV:");
+		textCV = new JTextField();
+		spinnerPuertas = new JSpinner();
+		comboBoxCombustible = new JComboBox();
+		lblColor = new JLabel("Color");
+		textMatricula = new JTextField();
+		setComponetProperties();
+		setComponentAdapters();
+	}
+
+	private void setComponentAdapters() {
 		btnCliente.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseClicked(MouseEvent e){
-					Cliente Ventana = new Cliente();
-					Ventana.getFrame().setVisible(true);
-					frame.dispose();
+			public void mouseClicked(MouseEvent e) {
+				Cliente Ventana = new Cliente();
+				Ventana.getFrame().setVisible(true);
+				frame.dispose();
 			}
 		});
-		
-		btnReparacion = new JButton("Reparar");
-		btnReparacion.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
-		btnReparacion.setBounds(398, 196, 135, 78);
-		frame.getContentPane().add(btnReparacion);
-		
-		btnGuardar = new JButton("Guardar");
-		btnGuardar.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
-		btnGuardar.setBounds(31, 396, 135, 53);
-		frame.getContentPane().add(btnGuardar);
 
-		btnEditar = new JButton("Editar");
-		btnEditar.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
-		btnEditar.setBounds(214, 396, 135, 53);
-		frame.getContentPane().add(btnEditar);
-		
-		buttonLimpiar = new JButton("Limpiar");
 		buttonLimpiar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				clearTxtField();
 			}
 		});
-		buttonLimpiar.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
-		buttonLimpiar.setBounds(398, 396, 135, 53);
-		frame.getContentPane().add(buttonLimpiar);
-		
 	}
-	
-	/**
-	 * Datos usuario
-	 */
-	private void setComponentProperties(){
-		lblMatricula_1 = new JLabel("Matricula:");
-		lblMatricula_1.setBounds(31, 29, 79, 16);                 
+
+	private void setComponetProperties() {
+		frame.setBounds(100, 100, 555, 493);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Vehiculo");
+		frame.getContentPane().setLayout(null);
+
+		lblMatricula_1.setBounds(31, 29, 79, 16);
 		frame.getContentPane().add(lblMatricula_1);
-		
-		textNombre = new JTextField();
+
 		textNombre.setBounds(154, 24, 171, 26);
 		frame.getContentPane().add(textNombre);
 		textNombre.setColumns(10);
-		
-		lblMarca = new JLabel("Marca:");
+
 		lblMarca.setBounds(31, 66, 61, 16);
 		frame.getContentPane().add(lblMarca);
-		
-		textMarca = new JTextField();
+
 		textMarca.setBounds(154, 61, 171, 26);
 		frame.getContentPane().add(textMarca);
 		textMarca.setColumns(10);
-		
-		lblModelo = new JLabel("Modelo:");
+
 		lblModelo.setBounds(31, 104, 61, 16);
 		frame.getContentPane().add(lblModelo);
-		
-		textModelo = new JTextField();
+
 		textModelo.setBounds(154, 99, 171, 26);
 		frame.getContentPane().add(textModelo);
 		textModelo.setColumns(10);
-		
-		lblCV = new JLabel("CV:");
+
 		lblCV.setBounds(31, 141, 71, 16);
 		frame.getContentPane().add(lblCV);
-		
-		textCV = new JTextField();
+
 		textCV.setBounds(154, 136, 171, 26);
 		frame.getContentPane().add(textCV);
 		textCV.setColumns(10);
-		
+
 		lblPuertas = new JLabel("Puertas:");
 		lblPuertas.setBounds(31, 182, 61, 16);
 		frame.getContentPane().add(lblPuertas);
-		
-		spinnerPuertas = new JSpinner();
+
 		spinnerPuertas.setBounds(154, 177, 79, 26);
 		frame.getContentPane().add(spinnerPuertas);
-		model = new SpinnerNumberModel(
-				new Integer(0), // Dato visualizado al inicio en el spinner 
-				new Integer(0), // Límite inferior 
-				new Integer(10), // Límite superior 
-				new Integer(1)); // incremento-decremento 
+		model = new SpinnerNumberModel(new Integer(0), // Dato visualizado al
+														// inicio en el spinner
+				new Integer(0), // Límite inferior
+				new Integer(10), // Límite superior
+				new Integer(1)); // incremento-decremento
 		spinnerPuertas.setModel(model);
-		
-		
-		lblColor = new JLabel("Color:");
+
 		lblColor.setBounds(31, 217, 61, 16);
 		frame.getContentPane().add(lblColor);
-		
-		textColor = new JTextField();
+
 		textColor.setBounds(154, 215, 171, 26);
 		frame.getContentPane().add(textColor);
 		textColor.setColumns(10);
-		
+
 		lblCombustible = new JLabel("Combustible:");
 		lblCombustible.setBounds(23, 258, 87, 16);
 		frame.getContentPane().add(lblCombustible);
-		
-		comboBoxCombustible = new JComboBox();
+
 		comboBoxCombustible.setBounds(154, 254, 171, 27);
 		frame.getContentPane().add(comboBoxCombustible);
 		comboBoxCombustible.addItem("Diesel");
 		comboBoxCombustible.addItem("Gasolina");
 		comboBoxCombustible.addItem("Electrico");
-		
+
 		lblAoMatriculacion = new JLabel("Año Matriculacion:");
 		lblAoMatriculacion.setBounds(23, 303, 118, 16);
 		frame.getContentPane().add(lblAoMatriculacion);
-		
+
 		spinnerAnioMatricula = new JSpinner();
 		spinnerAnioMatricula.setBounds(154, 298, 79, 26);
 		frame.getContentPane().add(spinnerAnioMatricula);
-		model1 = new SpinnerNumberModel(
-				new Integer(1950), // Dato visualizado al inicio en el spinner 
-				new Integer(1950), // Límite inferior 
-				new Integer(2017), // Límite superior 
-				new Integer(1)); // incremento-decremento 
+		model1 = new SpinnerNumberModel(new Integer(1950), // Dato visualizado
+															// al inicio en el
+															// spinner
+				new Integer(1950), // Límite inferior
+				new Integer(2017), // Límite superior
+				new Integer(1)); // incremento-decremento
 		spinnerAnioMatricula.setModel(model1);
-		
-		
 
+		btnCliente.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		btnCliente.setBounds(398, 66, 135, 78);
+		frame.getContentPane().add(btnCliente);
+
+		btnReparacion.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		btnReparacion.setBounds(398, 196, 135, 78);
+		frame.getContentPane().add(btnReparacion);
+
+		btnGuardar.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		btnGuardar.setBounds(31, 396, 135, 53);
+		frame.getContentPane().add(btnGuardar);
+
+		btnEditar.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		btnEditar.setBounds(214, 396, 135, 53);
+		frame.getContentPane().add(btnEditar);
+
+		buttonLimpiar.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		buttonLimpiar.setBounds(398, 396, 135, 53);
+		frame.getContentPane().add(buttonLimpiar);
 	}
-	//metodo para limpiar el formulario
-	public void clearTxtField(){
-			textNombre.setText("");
-			textMarca.setText("");
-			textModelo.setText("");
-			textMatricula.setText("");
-			textMatricula.setText("");
-			spinnerPuertas.setValue(0);
-			textColor.setText("");
-			comboBoxCombustible.setSelectedItem("Diesel");
-			spinnerAnioMatricula.setValue(1950);
-		}
+
+	/**
+	 * Algo
+	 */
+	public void clearTxtField() {
+		textNombre.setText("");
+		textMarca.setText("");
+		textModelo.setText("");
+		textMatricula.setText("");
+		textCV.setText("");
+		spinnerPuertas.setValue(0);
+		textColor.setText("");
+		comboBoxCombustible.setSelectedItem("Diesel");
+		spinnerAnioMatricula.setValue(1950);
+	}
 }
