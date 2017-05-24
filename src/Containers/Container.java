2b2a8dbaddb97b61e99c7b1f5303858b05a94bf2
sergio <sergio_pla_11@hotmail.com>
2017-05-24 13:23:27 +0200

    Clase container creada

---
+++ src/Containers/Container.java
@@ -0,0 +1,12 @@
+package Containers;
+
+import java.util.Collection;
+
+import Models.Usuario;
+
+public class Container {
+	public static Collection<Models.Vehiculo> listaVehiculos;
+	public static Usuario mecanicoActivo;
+	public static Collection<Models.Reparar> listaReparaciones;
+
+}
