fe3dd012a0321d734bd94bdac6f99310cdfc91f5
sergio <sergio_pla_11@hotmail.com>
2017-05-31 17:58:21 +0200

    Script modificado

--- src/script/taller.sql
+++ src/script/taller.sql
@@ -1,41 +1,42 @@
- create table Cliente(
-DNI varchar2(9)primary key not null,
-Nombre varchar2(20) not null,
-Apellido varchar2(20) not null,
-Teléfono int not null,
-Email varchar2(30) not null,
-direccion varchar2(30) not null
+create table Cliente(
+DNI varchar(9)primary key not null,
+Nombre varchar(20),
+Apellido varchar(20),
+Telefono int,
+Email varchar(30),
+direccion varchar(30)
 )
 
-create table Vehículo(
-Matricula varchar2(15)primary key not null,
-Marca varchar2(20),
-Modelo varcahr2(20),
+create table Vehiculo(
+Matricula varchar(15)primary key not null,
+Marca varchar(20),
+Modelo varchar(20),
 Puertas int,
-Color varchar2(15),
-Combustiblevarchar2(15),
-Año_Matriculacion date,
+Color varchar(15),
+Combustible varchar(15),
+Anio_Matriculacion date,
 CV int,
-DNI_Cliente varchar2(9) references Cliente 
+DNI_Cliente varchar(9) references Cliente 
 )
 CREATE TABLE MECANICO(
-Usuario varchar2(20) primary Key NOT NULL,
-Contraseña varchar2(20) NOT NULL,
-Nombre varchar2(25) NOT NULL,
-Apellidos varchar2(50) NOT NULL,
-Dias_Vacaciones int NOT NULL,
-Sueldo float NOT NULL,
-Fecha_Contratacion date NOT NULL
+Usuario varchar(20) primary Key NOT NULL,
+Contrasenia varchar(20) NOT NULL,
+Nombre varchar(25),
+Apellidos varchar(50),
+Dias_Vacaciones int,
+Sueldo float,
+Fecha_Contratacion date
 )
-CREATE TABLE REPARACIÓN(
-Matricula varchar2(15) references vehículo not null,
-Cod_Reparación varchar2(10) primary key not null,
-detalles varchar2(100),
-fecha_Entrada date,
-fecha_Salida date,
+CREATE TABLE REPARACION(
+Matricula varchar(15) references vehiculo not null,
+Cod_Reparacion varchar(10) primary key not null,
+detalles varchar(100),
+fecha_entrada date,
+fecha_salida date,
+inicio_reparacion date,
+fin_reparacion date,
 Precio float,
-usuario_mecanico varchar2(20) references MECANICO NOT NULL,
-estado varchar2(20) not null
-
-
+Precio_reparacion float,
+usuario_mecanico varchar(20) references MECANICO,
+estado varchar(20)
 )
